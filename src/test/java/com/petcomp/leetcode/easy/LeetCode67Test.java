d015a7bcde72b5760d37e92d4bbff235f17de8c7
songwei.liu <songv.liu@gmail.com>
2021-09-20 22:42:59 +0800

    67

---
+++ src/test/java/com/petcomp/leetcode/easy/LeetCode67Test.java
@@ -0,0 +1,15 @@
+package com.petcomp.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+public class LeetCode67Test {
+
+    @Test
+    public void testBinaryAdd(){
+
+        BinaryAdd67 solution = new BinaryAdd67();
+        String result = solution.addBinary("0", "0");
+        System.out.println(result);
+
+    }
+}
